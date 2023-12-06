3b1a4cc4d20bf4008f6ed33e43f26e216c0db7de
Fabio Minelli <fabio.minelli@hpe.com>
2023-12-07 00:26:18 +0100

    Day 5 2023: use Long (no need for BigInteger)

--- src/main/kotlin/AoC23Day05.kt
+++ src/main/kotlin/AoC23Day05.kt
@@ -1,4 +1,5 @@
 import java.math.BigInteger
+import kotlin.math.min
 
 class AoC23Day05: DayProblemSolver(5, 2023) {
 
@@ -13,10 +14,10 @@ class AoC23Day05: DayProblemSolver(5, 2023) {
 
     override fun getFirstStarOutcome(lines: List<String>): Int {
 
-        val seeds = lines[0].split(":")[1].trim().split(" ").map { it.trim().toBigInteger() }
+        val seeds = lines[0].split(":")[1].trim().split(" ").map { it.trim().toLong() }
         val maps = parseAllCategoryMaps(lines)
 
-        fun findLocation(seed:BigInteger):BigInteger {
+        fun findLocation(seed:Long):Long {
             return categories.fold(seed) { acc, cat ->
                 val rangesForCategory = maps[cat]!!
                 rangesForCategory.firstNotNullOfOrNull { r -> r.mapValue(acc) } ?: acc
@@ -34,20 +35,20 @@ class AoC23Day05: DayProblemSolver(5, 2023) {
     override fun getSecondStarOutcome(lines: List<String>): Int {
 
         // Parsing input
-        val sequence = lines[0].split(":")[1].trim().split(" ").map { it.trim().toBigInteger() }
+        val sequence = lines[0].split(":")[1].trim().split(" ").map { it.trim().toLong() }
         val sourceRanges = (0..<sequence.size/2).map { idx ->
                                 Pair(sequence[2*idx],sequence[2*idx+1])
                             }
         val maps = parseAllCategoryMaps(lines)
 
-        fun findLocationAndDelta(seedRange:Pair<BigInteger,BigInteger>):Pair<BigInteger,BigInteger> {
+        fun findLocationAndDelta(seedRange:Pair<Long,Long>):Pair<Long,Long> {
             return categories.fold(seedRange) { acc, cat ->
                 val rangesForCategory = maps[cat]!!
                 rangesForCategory.firstNotNullOfOrNull { r -> r.mapValueWithDelta(acc) } ?: acc
             }
         }
 
-        var minimumLocation = BigInteger.ZERO
+        var minimumLocation = 0.toLong()
 
         sourceRanges.forEach { range ->
             // instead of transforming every possible seed (it would take too long) we try to transform
@@ -64,7 +65,7 @@ class AoC23Day05: DayProblemSolver(5, 2023) {
             var seed = range.first
             while (seed<range.first+range.second) {
                 val locationAndDelta = findLocationAndDelta(Pair(seed,range.first+range.second-seed))
-                if (minimumLocation==BigInteger.ZERO || locationAndDelta.first<minimumLocation)
+                if (minimumLocation== 0.toLong() || locationAndDelta.first<minimumLocation)
                     minimumLocation = locationAndDelta.first
                 seed += locationAndDelta.second
             }
@@ -79,19 +80,19 @@ class AoC23Day05: DayProblemSolver(5, 2023) {
 
     // Represent an individual range conversion inside a map (e.g. any range in 'seed-to-soil')
     class CategoryRange(line:String) {
-        private val destStart:BigInteger
-        private val srcStart: BigInteger
-        private val len:BigInteger
+        private val destStart:Long
+        private val srcStart: Long
+        private val len:Long
         init {
             val (d,s,l) = line.split(" ")
-            destStart = d.toBigInteger()
-            srcStart = s.toBigInteger()
-            len = l.toBigInteger()
+            destStart = d.toLong()
+            srcStart = s.toLong()
+            len = l.toLong()
         }
 
         // If the provided value is included in the range, provide the transformed value,
         // otherwise null
-        fun mapValue(value:BigInteger):BigInteger? {
+        fun mapValue(value:Long):Long? {
             return if ( (srcStart..<srcStart+len).contains(value)) {
                 destStart + (value-srcStart)
             } else {
@@ -106,10 +107,10 @@ class AoC23Day05: DayProblemSolver(5, 2023) {
         //   - what is the length of the range of values starting from 100 that can be transformed applying the
         //     same addition (so, if 100 became 100+x, all values in the range can be transformed by adding x)
         //     the length *cannot* be larger than the delta passed by the caller
-        fun mapValueWithDelta(src:Pair<BigInteger,BigInteger>):Pair<BigInteger,BigInteger>? {
+        fun mapValueWithDelta(src:Pair<Long,Long>):Pair<Long,Long>? {
             return if ( (srcStart..<srcStart+len).contains(src.first)) {
                 val newValue = destStart + (src.first-srcStart)
-                val newDelta = src.second.min(srcStart+len-src.first)
+                val newDelta = min(src.second, srcStart+len-src.first)
                 Pair(newValue, newDelta)
             } else {
                 null
