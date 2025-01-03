7a5d0dca6e92765ca05d782cd2ed30dc04f251a2
fminelli <fabio.minelli@hpe.com>
2025-01-03 22:00:05 +0100

    AoC 24, days 20,22

--- src/main/kotlin/aoc24/AoC24Day20.kt
+++ src/main/kotlin/aoc24/AoC24Day20.kt
@@ -3,7 +3,95 @@ package aoc24
 import Problem
 import kotlin.math.abs
 
-class AoC24Day20: Problem(20, 2024, "?") {
+class AoC24Day20: Problem(20, 2024, "Race Condition") {
+
+    data class Position(val x:Int, val y:Int)
+
+    enum class Direction(val dx:Int, val dy:Int) {
+        UP(0, -1), DOWN(0, 1), LEFT(-1, 0), RIGHT(1, 0);
+    }
+
+    enum class CellType(val ch:Char) {
+        WALL('#'), EMPTY('.'), START('S'), FINISH('E');
+
+        companion object {
+            fun of(ch:Char) = entries.find { it.ch == ch }!!
+        }
+    }
+
+    class Racetrack(lines:List<String>) {
+
+        private var start:Position? = null
+        private var finish:Position? = null
+
+        val grid = Array (lines[0].length) { x ->
+            Array (lines.size) { y ->
+                CellType.of(lines[y][x]).also {
+                    if (it == CellType.START) start = Position(x, y)
+                    if (it == CellType.FINISH) finish = Position(x, y)
+                }
+            }
+        }
+        val width = grid[0].size
+        val height = grid.size
+
+        private val distance:MutableMap<Position, Int> = mutableMapOf()
+        private val sequence:MutableList<Position> = mutableListOf()
+
+        init {
+            distance[start!!] = 0
+            sequence.add(start!!)
+            var step = 0
+            var current = start!!
+            var prev:Position? = null
+            while(true) {
+                step++
+                val nextPos = Direction.entries.firstNotNullOf { dir ->
+                    val next = Position(current.x + dir.dx, current.y + dir.dy)
+                    if (next==prev || grid[next.x][next.y] == CellType.WALL) null else next
+                }
+                distance[nextPos] = step
+                sequence.add(nextPos)
+                if (nextPos == finish) break
+                prev = current
+                current = nextPos
+            }
+        }
+
+        fun countShortCheatsSavingAtLeast(minimum:Int):Int {
+            return (0..sequence.size-minimum).sumOf { step ->
+                val pos = sequence[step]
+                Direction.entries.count { dir ->
+                    val target = Position(pos.x + 2*dir.dx, pos.y + 2*dir.dy)
+                    val ok = target.x >= 0 && target.x < grid.size && target.y >= 0 && target.y < grid[0].size
+                        && grid[target.x][target.y] != CellType.WALL
+                        && distance[target]!! - (distance[pos]!! + 2) >= minimum
+                    ok
+                }
+            }
+        }
+
+
+        fun countLongCheatsSavingAtLeast(minimum:Int):Int {
+
+            val totalCheats = (0..sequence.size - minimum).sumOf { step ->
+
+                val pos = sequence[step]
+                (-20..20).sumOf { dx ->
+                    (-20+abs(dx)..20-abs(dx)).count { dy ->
+                        if ((0..<width).contains(pos.x+dx) && (0..<height).contains(pos.y+dy)) {
+                            val target = Position(pos.x + dx, pos.y + dy)
+                            (grid[target.x][target.y] != CellType.WALL
+                                    && distance[target]!! - distance[pos]!! - abs(dx) - abs(dy) >= minimum)
+                        } else false
+                    }
+                }
+            }
+
+            return totalCheats
+        }
+
+    }
 
 
     //=======================
@@ -11,7 +99,8 @@ class AoC24Day20: Problem(20, 2024, "?") {
     //=======================
 
     override fun getFirstStarOutcome(lines: List<String>): String {
-        return "n/a"
+        val racetrack = Racetrack(lines)
+        return racetrack.countShortCheatsSavingAtLeast(if (isSample) 12 else 100).toString()
     }
 
 
@@ -22,7 +111,8 @@ class AoC24Day20: Problem(20, 2024, "?") {
 
 
     override fun getSecondStarOutcome(lines: List<String>): String {
-        return "n/a"
+        val racetrack = Racetrack(lines)
+        return racetrack.countLongCheatsSavingAtLeast(if (isSample) 70 else 100).toString()
     }
 
 }
--- src/main/kotlin/aoc24/AoC24Day22.kt
+++ src/main/kotlin/aoc24/AoC24Day22.kt
@@ -1,9 +1,52 @@
 package aoc24
 
 import Problem
-import kotlin.math.abs
 
-class AoC24Day22: Problem(22, 2024, "?") {
+class AoC24Day22: Problem(22, 2024, "Monkey Market") {
+
+    val iterations = 2000
+
+    private val pruneVal = 16777216
+    private fun nextSecret(n: Long): Long {
+        var n1 = n
+        n1 = (n1 xor (n1 * 64)) % pruneVal
+        n1 = (n1 xor (n1 / 32)) % pruneVal
+        n1 = (n1 xor (n1 * 2048)) % pruneVal
+        return n1
+    }
+
+
+    private fun secretNumber(initial:Int):Long {
+        var n = initial.toLong()
+        for (i in 1..iterations) {
+            n = nextSecret(n)
+        }
+        return n
+    }
+
+    private fun maximumBananas(initialSecrets:List<Int>):Int {
+        val changesMap:MutableMap<List<Int>,Int> = mutableMapOf()
+        initialSecrets.forEach { initial ->
+            val secretChangesMap:MutableMap<List<Int>,Int> = mutableMapOf()
+            var n = initial.toLong()
+            var price = n.mod( 10)
+            var changes:List<Int> = emptyList()
+            for (i in 1..iterations) {
+                val n1 = nextSecret(n)
+                val prevPrice = price
+                price = n1.mod( 10)
+                changes = changes.drop(if (changes.size==4) 1 else 0).plus(price-prevPrice)
+                if (changes.size == 4 && !secretChangesMap.containsKey(changes)) {
+                    secretChangesMap[changes] = price
+                }
+                n = n1
+            }
+            secretChangesMap.forEach { (changes,price) ->
+                changesMap[changes] = (changesMap[changes]?:0) + price
+            }
+        }
+        return changesMap.maxOf { it.value }
+    }
 
 
     //=======================
@@ -11,7 +54,7 @@ class AoC24Day22: Problem(22, 2024, "?") {
     //=======================
 
     override fun getFirstStarOutcome(lines: List<String>): String {
-        return "n/a"
+        return lines.map { it.toInt() }.map { secretNumber(it) }.sumOf { it }.toString()
     }
 
 
@@ -22,7 +65,7 @@ class AoC24Day22: Problem(22, 2024, "?") {
 
 
     override fun getSecondStarOutcome(lines: List<String>): String {
-        return "n/a"
+        return maximumBananas(lines.map { it.toInt() }).toString()
     }
 
 }
